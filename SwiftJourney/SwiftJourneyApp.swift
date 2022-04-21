411443f6cf0c3934287e376eb09516ba8cc4ed3f
Hyeon Sang Lee <88080251+phainestha1@users.noreply.github.com>
2022-04-21 18:03:08 +0900

    Initial Commit

---
+++ SwiftJourney/SwiftJourneyApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftJourneyApp.swift
+//  SwiftJourney
+//
+//  Created by Noah's Ark on 2022/04/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftJourneyApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
